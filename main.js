d360dc746ca57f51aeeb172e1495178d68dab559
yyyh <444510796@qq.com>
2019-04-22 15:45:07 +0800

    1

---
+++ main.js
@@ -0,0 +1,24 @@
+var result = `
+面试官您好，我是xx
+下面我将用动画的形式来介绍自己
+
+只用文字介绍太单调了
+那就用代码来显示吧
+
+首先准备一些样式
+
+html{
+    background: #ddd;
+    font-size: 20px;
+}
+`
+
+var n = 0
+var id = setInterval(()=>{
+    n += 1
+    code.innerHTML = result.substring(0,n)
+    styleTag.innerHTML = result.substring(0,n)
+    if(n >= result.length){
+        window.clearInterval(id)
+    }
+},10)
